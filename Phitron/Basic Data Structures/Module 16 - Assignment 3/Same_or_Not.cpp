bf07aba33064cf51746b398452dea203ad04ea74
Sojib Hossain <sojibhossainbd75@gmail.com>
2025-06-30 20:18:07 +0600

    Modified

---
+++ Phitron/Basic Data Structures/Module 16 - Assignment 3/Same_or_Not.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    stack<int> st;
+    queue<int> qu;
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin >> x;
+        qu.push(x);
+    }
+
+    bool same = true;
+
+    while (!st.empty() && !qu.empty())
+    {
+        if (st.top() != qu.front())
+        {
+            same = false;
+            break;
+        }
+        st.pop();
+        qu.pop();
+        
+    }
+
+    if (!empty)
+    {
+        
+    }
+    
+    
+
+    if (same) cout << "YES";
+    else cout << "NO";
+
+    
+    
+    
+
+    return 0;
+}
