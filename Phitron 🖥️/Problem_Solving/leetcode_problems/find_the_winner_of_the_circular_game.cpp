152033e3ff2f11160f7bf61a512b53f6e6ecc215
Sojib Hossain <sojibhossainbd75@gmail.com>
2025-08-14 00:32:35 +0600

    file modified

---
+++ Phitron 🖥️/Problem_Solving/leetcode_problems/find_the_winner_of_the_circular_game.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    queue<int> q;
+    for (int i = 1; i <=n; i++)
+    {
+        q.push(i);
+    }
+    
+    
+
+    return 0;
+}
