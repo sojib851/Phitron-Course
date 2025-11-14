e350128fa93cb732d56ad134921cda3cbd03c28a
Sojib Hossain <sojibhossainbd75@gmail.com>
2025-11-14 15:43:33 +0600

    backup

---
+++ Phitron /Phitron/Basic Data Structure/Module 17/backup.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = 1; i <= a; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= b; i++)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
